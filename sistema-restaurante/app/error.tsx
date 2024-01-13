bc48c390fcf348a416c04710ad95bbcfcbd72aea
hennanlewis <39320310+hennanlewis@users.noreply.github.com>
2024-01-13 04:28:46 -0300

    feat: redirect error page to dashboard

--- sistema-restaurante/app/error.tsx
+++ sistema-restaurante/app/error.tsx
@@ -2,6 +2,7 @@
 import { useEffect } from "react"
 
 import style from "./style.module.css"
+import { useRouter } from "next/navigation"
 
 export default function Error({
 	error,
@@ -10,13 +11,16 @@ export default function Error({
 	error: Error & { digest?: string }
 	reset: () => void
 }) {
+    const router = useRouter()
+    const toDashboard = () => router.replace("/dashboard")
+
 	useEffect(() => console.error(error), [error])
 
 	return (
 		<main className={style.error}>
 			<h1 className={style.errorTitle}>Eita!</h1>
 			<h2>Desculpe, mas correu um erro</h2>
-			<button className={style.errorButton} onClick={() => reset()}>
+			<button className={style.errorButton} onClick={toDashboard}>
 				Carregar novamente
 			</button>
 		</main>
