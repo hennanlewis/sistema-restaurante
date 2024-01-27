e9d23b12a4e9fa70503dcd196edbbe60700aae15
hennanlewis <39320310+hennanlewis@users.noreply.github.com>
2024-01-27 01:50:12 -0300

    feat: removed ambiguous files

--- sistema-restaurante/app/api/casdastrar-acesso/route.ts
+++
@@ -1,8 +0,0 @@
-import { insertUser } from "@/db/db"
-import { NextRequest, NextResponse } from "next/server"
-
-export async function POST(req: NextRequest) {
-	const user = await req.json()
-	const result = await insertUser(user)
-	return NextResponse.json(result)
-}
