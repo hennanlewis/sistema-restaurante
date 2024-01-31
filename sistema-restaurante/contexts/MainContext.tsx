f91a917d5f7ffe1b502f69920daf2b953428359a
hennanlewis <39320310+hennanlewis@users.noreply.github.com>
2024-01-31 06:29:23 -0300

    feat: created orders with id

--- sistema-restaurante/contexts/MainContext.tsx
+++ sistema-restaurante/contexts/MainContext.tsx
@@ -2,64 +2,69 @@
 import { useCustomHook } from "@/hooks/customHook"
 import { usePathname, useRouter } from "next/navigation"
 import React, {
-	createContext,
-	useContext,
-	useState,
-	ReactNode,
-	SetStateAction,
-	Dispatch,
-	useEffect
+    createContext,
+    useContext,
+    useState,
+    ReactNode,
+    SetStateAction,
+    Dispatch,
+    useEffect
 } from "react"
 
 type ContextProps = {
-	user: UserData | null
-	setUser: Dispatch<SetStateAction<UserData | null>>
-	orders: OrderData[]
-	setOrders: Dispatch<SetStateAction<OrderData[]>>
-	notOrderedStack: OrderData[]
-	setNotOrderedStack: Dispatch<SetStateAction<OrderData[]>>
-	restaurantTables: RestaurantTableData[]
-	setRestaurantTables: Dispatch<SetStateAction<RestaurantTableData[]>>
+    user: UserData | null
+    setUser: Dispatch<SetStateAction<UserData | null>>
+    orders: OrderData[]
+    ordersWithID: OrderData[]
+    setOrders: Dispatch<SetStateAction<OrderData[]>>
+    notOrderedStack: OrderData[]
+    setNotOrderedStack: Dispatch<SetStateAction<OrderData[]>>
+    restaurantTables: RestaurantTableData[]
+    setRestaurantTables: Dispatch<SetStateAction<RestaurantTableData[]>>
 }
 
 const RestaurantTableContext = createContext<ContextProps | undefined>(undefined)
 
 export const TableContextProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-	const [orders, setOrders] = useState<OrderData[]>([])
-	const [user, setUser] = useState<UserData | null>(null)
-	const [notOrderedStack, setNotOrderedStack] = useState<OrderData[]>([])
-	const [restaurantTables, setRestaurantTables] = useState<RestaurantTableData[]>([])
-	const [isLoading, setIsLoading] = useState(true)
+    const [orders, setOrders] = useState<OrderData[]>([])
+    const [user, setUser] = useState<UserData | null>(null)
+    const [notOrderedStack, setNotOrderedStack] = useState<OrderData[]>([])
+    const [restaurantTables, setRestaurantTables] = useState<RestaurantTableData[]>([])
+    const [isLoading, setIsLoading] = useState(true)
 
-	const router = useRouter()
-	const pathname = usePathname()
-	const customHook = useCustomHook()
+    const ordersWithID = orders.filter(order => order._id)
+    if (ordersWithID.length > 0) console.log("Pedidos com ID", ordersWithID)
 
-	useEffect(() => {
-		setIsLoading(true)
-		if (customHook) setUser(customHook)
-		if (pathname == "/" && user) router.push("/dashboard")
-		if (!user) router.push("/")
-		setIsLoading(false)
-	}, [user, pathname])
+    const router = useRouter()
+    const pathname = usePathname()
+    const customHook = useCustomHook()
 
-	return (
-		<RestaurantTableContext.Provider value={{
-			user, setUser,
-			orders, setOrders,
-			notOrderedStack,
-			setNotOrderedStack,
-			restaurantTables, setRestaurantTables
-		}}>
-			{!isLoading && children}
-		</RestaurantTableContext.Provider>
-	)
+    useEffect(() => {
+        setIsLoading(true)
+        if (customHook) setUser(customHook)
+        if (pathname == "/" && user) router.push("/dashboard")
+        if (!user) router.push("/")
+        setIsLoading(false)
+    }, [user, pathname])
+
+    return (
+        <RestaurantTableContext.Provider value={{
+            user, setUser,
+            ordersWithID,
+            orders, setOrders,
+            notOrderedStack,
+            setNotOrderedStack,
+            restaurantTables, setRestaurantTables
+        }}>
+            {!isLoading && children}
+        </RestaurantTableContext.Provider>
+    )
 }
 
 export const useBaseContext = (): ContextProps => {
-	const contexto = useContext(RestaurantTableContext)
-	if (!contexto) {
-		throw new Error("useBaseContext deve ser utilizado dentro de um TableContextProvider")
-	}
-	return contexto
+    const contexto = useContext(RestaurantTableContext)
+    if (!contexto) {
+        throw new Error("useBaseContext deve ser utilizado dentro de um TableContextProvider")
+    }
+    return contexto
 }
