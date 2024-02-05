3d4de414c08de2a90a13a7df64f4511099379c8b
Hennan Lewis <39320310+hennanlewis@users.noreply.github.com>
2024-02-05 07:43:46 -0300

    feat: adiciona botão de voltar e formata tabulação

--- sistema-restaurante/app/mesas/components/MainComponent/index.tsx
+++ sistema-restaurante/app/mesas/components/MainComponent/index.tsx
@@ -1,36 +1,44 @@
 "use client"
 import { useEffect } from "react"
+import { IoMdArrowBack } from "react-icons/io"
+
 import { useBaseContext } from "@/contexts/MainContext"
 
+import style from "../../restaurante.module.css"
 import { TableBody } from "../TableBody"
 import { TableHead } from "../TableHead"
-import style from "../../restaurante.module.css"
+import { useRouter } from "next/navigation"
 
 type MainComponentProps = {
-	restaurantTablesFromAPI: RestaurantTableData[]
+    restaurantTablesFromAPI: RestaurantTableData[]
     restaurantOrdersFromAPI: OrderData[]
 }
 
 export function MainComponent({ restaurantTablesFromAPI, restaurantOrdersFromAPI }: MainComponentProps) {
-	const { restaurantTables, setRestaurantTables, orders, setOrders  } = useBaseContext()
+    const { restaurantTables, setRestaurantTables, orders, setOrders } = useBaseContext()
+    const router = useRouter()
 
-	useEffect(() => {
-		setRestaurantTables(currentValues => restaurantTables.length == 0 ? restaurantTablesFromAPI : currentValues)
+    const mainPage = () => router.back()
+
+    useEffect(() => {
+        setRestaurantTables(currentValues => restaurantTables.length == 0 ? restaurantTablesFromAPI : currentValues)
         setOrders(currentValues => orders.length == 0 ? restaurantOrdersFromAPI : currentValues)
-	}, [])
+    }, [])
 
-
-	return (
-		<main className={style.main}>
-			<div className={style.table}>
-				<TableHead />
-				<TableBody />
-				{/* <div className={style.orders}>
+    return (
+        <main className={style.main}>
+            <div className={style.table}>
+                <div className={style.topOptions}>
+                    <button onClick={mainPage}><IoMdArrowBack /><span>Voltar</span></button>
+                </div>
+                <TableHead />
+                <TableBody />
+                {/* <div className={style.orders}>
 					{currencyFormater(restaurantTables
 						.reduce((sum, restaurantTable) => sum += restaurantTable.orderValue, 0)
 					)}
 				</div> */}
-			</div>
-		</main>
-	)
+            </div>
+        </main>
+    )
 }
